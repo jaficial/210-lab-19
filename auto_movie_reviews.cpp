0239b96dfeff3981ea57f4ade13e01bc2b5a7f70
Jed Aficial's PC <jedaficial1@gmail.com>
2024-10-06 17:50:34 -0700

    10-minute update

--- auto_movie_reviews.cpp
+++ auto_movie_reviews.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-// CITED: Will be reusing some of my code from my previous lab submission (Lab 17) for linked list functions
- //
+// CITED: Will be reusing my Lab 17 submission, except editting it to fit Lab 19
 struct Node {
     float rating;
     string comments;
@@ -16,7 +15,6 @@ struct Node {
 
 void output(Node *);
 void add_front_node(Node *, Node *, string, int);
-void add_tail_node();
 void delete_linked_list(Node *, Node *);
 
 /* funtion takes in the head and current node of the linked list,
@@ -36,14 +34,6 @@ void add_front_node(Node *new_value, Node *head, string temp_comment, float temp
 	new_value->comments = temp_comment;
 	new_value->rating = temp_rating;
 }
-/*  add_tail_node takes in the new node, data for the new node, and the tail node,
-    and assigns the tail node to the new node */
-void add_tail_node(Node *new_value, Node *tail, string temp_comment, float temp_rating){
-    tail->next = new_value;
-    new_value->next = nullptr;
-	new_value->comments = temp_comment;
-	new_value->rating = temp_rating;
-}
 
 /* output function traverses the linked list and outputs the nodes of the linked list, 
    as well as calulates the average of the movie ratings. */ 
@@ -52,7 +42,6 @@ void output(Node * list){
         cout << "Empty list." << endl;
         return;
     }
-
     else{
         int count = 1;
         Node * current = list;
@@ -70,82 +59,46 @@ void output(Node * list){
     }
 }
 
+
+/* NOTES: - REVIEW COMMENTS NEED TO BE READ FROM AN EXTERNAL FILE
+          - MOVIE RATING IS A RANDOM (type double) NUMBER BETWEEN 1.0 and 5.0 (must only have 1 decimal place))
+          - NEED A MOVIE CLASS 
+            - OBJECTS: TITLE, 
+                LINKED LIST FOR RATING NUMBER 
+                LINKED LIST FOR REVIEW COMMENTS*/
 int main(){
     Node *head = nullptr; // node
     Node *current = head; // current points to head node
-    Node *tail = head; // tail node
-    
-    int choice;
-    cout << "Which linked list method should we use?" << endl;
-    cout << setw(4) << "" << "[1] New nodes are added at the head of the linked list" << endl;
-    cout << setw(4) << "" << "[2] New nodes are added at the tail of the linked list" << endl;
-    cout << setw(4) << "" << "Choice: ";
-    cin >> choice;
 
     bool more_reviews = 1; // if 0, while loop ends
     char temp_more_reviews;
     
-    if (choice == 1){ // adding node to the head of the linked list
-        while(more_reviews){
-            float temp_rating;
-            string temp_comment;
+    // adding node to the head of the linked list
+    while(more_reviews){
+        float temp_rating;
+        string temp_comment;
 
-            cout << "Enter review rating 0-5: ";
-            Node *new_value = new Node; // Node *new_value is in loop as a new one has to get defined for each new node
-            cin >> temp_rating;
-            cout << "Enter review comments: ";
-            cin.ignore();
-            getline(cin, temp_comment);
-        
-            if (!head){ // if its the first node
-                head = new_value;
-                new_value->next = nullptr;
-                new_value->rating = temp_rating;
-                new_value->comments = temp_comment;
-            }
-            else{ // if its not the first node
-                add_front_node(new_value, head, temp_comment, temp_rating);
-                head = new_value; // head node is now the most recently added node
-            }
-            cout << "Enter another review? Y/N: ";
-            cin >> temp_more_reviews;
-            if ((temp_more_reviews == 'n') || (temp_more_reviews == 'N')){
-                more_reviews = 0;
-            }
-        }
-   }
+        cout << "Enter review rating 0-5: ";
+        Node *new_value = new Node; // Node *new_value is in loop as a new one has to get defined for each new node
+        cin >> temp_rating;
+        cout << "Enter review comments: ";
+        cin.ignore();
+        getline(cin, temp_comment);
     
-    else { // adding node to the tail of the linked list, 
-	    /* NOTE: this needs to be formatted in a way where new value needs to 
-	     be pointing to a null in order to add to the tail */ 
-        while(more_reviews){
-            float temp_rating;
-            string temp_comment;
-            
-            cout << "Enter review rating 0-5: ";
-            Node *new_value = new Node;
-            cin >> temp_rating;
-            cout << "Enter review comments: ";
-            cin.ignore();
-            getline(cin, temp_comment);
-            
-            if (!head){ // in the case that it is the first node
-                new_value->next = nullptr;
-                new_value->rating = temp_rating;
-                new_value->comments = temp_comment;
-                head = new_value;
-                tail = head; // tail now has the data of the first node and points to nullptr
-            }
-            else{
-                add_tail_node(new_value, tail, temp_comment, temp_rating);
-                tail = new_value; // tail node is now the most recently added node
-            }
-
-            cout << "Enter another review? Y/N: ";
-            cin >> temp_more_reviews;
-            if ((temp_more_reviews == 'n') || (temp_more_reviews == 'N')){
-                more_reviews = 0;
-            }
+        if (!head){ // if its the first node
+            head = new_value;
+            new_value->next = nullptr;
+            new_value->rating = temp_rating;
+            new_value->comments = temp_comment;
+        }
+        else{ // if its not the first node
+            add_front_node(new_value, head, temp_comment, temp_rating);
+            head = new_value; // head node is now the most recently added node
+        }
+        cout << "Enter another review? Y/N: ";
+        cin >> temp_more_reviews;
+        if ((temp_more_reviews == 'n') || (temp_more_reviews == 'N')){
+            more_reviews = 0;
         }
     }
 
